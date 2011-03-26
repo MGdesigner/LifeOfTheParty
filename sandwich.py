1683fed23d24dbe4815978223b0ade16a41e76b4
Jack Wink <jackwink@umich.edu>
2011-03-26 10:39:15 -0400

    Gentlemen, sandwiches are on the way

---
+++ sandwich.py
@@ -0,0 +1,15 @@
+import facebook
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyACM0', 9600)
+ser.write('0')
+access_token = '201793946507565%7Cd202ecd7aef8dbce66e1e6e0-100002268818038%7CiHYYQSxjEu1_O2cltKLULG-7jL8' # LifeOf TheParty
+event_id = '130842913653732'  # Hackathon
+graph = facebook.GraphAPI(access_token)
+while 1:
+  posts = graph.get_connections(event_id, 'feed')['data']
+  last_status = posts[0]['message']
+  if last_status.find('sudo make me a sandwich') != -1:
+    ser.write('180')
+  sleep(5)
